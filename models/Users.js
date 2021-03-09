fce22aa64cb738adf91eb5e35fe77aed6a66179b
Marcelo Maia <70032966+mcmaia@users.noreply.github.com>
2021-03-09 07:27:01 -0300

    models with an error

---
+++ models/Users.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataType) =>  {
+    const User = sequelize.define('User', { 
+        id: {
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          name: {
+            type: DataType.STRING,
+            allowNull: false
+          },
+          email: DataType.STRING,
+          password: DataType.STRING,
+          createdAt: {
+            type: DataType.DATE
+          },
+          updatedAt: {
+            type: DataType.DATE
+          }
+    });
+    return User;
+}
---
+++ tests/testModelUser.js
@@ -0,0 +1,7 @@
+const { User } = require('../models');
+
+User.create({
+    name: "Marcelo",
+    email: "marcelo@maia.com",
+    password: "1234"
+});
